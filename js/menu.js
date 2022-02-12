e0efee174b43fc47dbb318317931b1bb70d35b99
mohammadshaban01 <43331502+mohammadshaban01@users.noreply.github.com>
2022-02-12 14:02:27 -0500

    new
    fix

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+
+//  $(function(){ 
+//     var navMain = $(".navbar-collapse");
+
+//     navMain.on("click", "a", null, function () {
+//         navMain.collapse('hide');
+//     });
+// });
+
+// $('.navbar-nav>li>a').on('click', function(){
+//     $('.navbar-collapse').collapse('hide');
+// });
